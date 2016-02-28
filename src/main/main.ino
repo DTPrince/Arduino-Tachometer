ce093a45b62eee24604a23ddd02764b096abaeb3
Derek Prince <ace.prince08@gmail.com>
2016-02-27 18:21:52 -0700

    Just perusing libs...

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,9 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-
+  // initialization checks, probably. Check logic level of signal in, engine running or not, etc.
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // 
 
 }
