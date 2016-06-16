7446d29a803ee0fc788d89deb65abfdeca477823
Derek Prince <ace.prince08@gmail.com>
2016-06-15 18:31:30 -0600

    Just adjusting some things as I prepare to merge this code with LED

--- src/LCD/LCD.ino
+++ src/LCD/LCD.ino
@@ -12,20 +12,22 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //shifter to the latter pins to avoid pi
 void setup() {
   // initialization checks, probably. Check logic level of signal in, engine running or not, etc.
   pinMode(pin, INPUT);  //reading period from pin(2) via PulseIn
-  
+
   lcd.begin(16,2);  // I don't actually know the dimensions of my screen yet, but these are popular dimensions so why not.
-                    // As for why on earth this library is column, row I will never know. Probably due to hardware-interface addressing but w/e. 
+                    // As for why on earth this library is column, row I will never know. Probably due to hardware-interface addressing but w/e.
 }
 
 void loop() {
-  // 
+  //
 
   //Might loop the loop and use a known number of pulseIn's to set the real-time duration of the loop.
   period = pulseIn(PIN_FREQ, HIGH, 10000); // read period from high logic. Defaulf of 1 (s) is pretty terrible, I'll have to come up with something.
                                       // 10000 = 10 ms sampling
                                       // Also, I have to make sure that the wave I'm reading is actually at TTL levels. That's slightly important.
   freq = 1/period;  //Might have to check against period == 0 or error handle the black hole.
-                    // This bit of code is also likely to disappear or be absorbed into the a functional implementation so I don't actaully have to dedicate memory to it.
+                    // This bit of code is also likely to disappear or be absorbed into the a functional implementation so I don't actually have to dedicate memory to it.
                     //  ^ depends on how often I need to call freq for readout.
-                    
+                    // I could write a small algorithm to check the diff/period ratio and have it sample more frequently if the engine is accelerating.
+                    // This might be too expensive but it would make the write cycles like a billion times less intensive. Usually.
+
   LiquidCrystal
