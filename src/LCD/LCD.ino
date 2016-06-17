89898d8af348173fe5778cd1159406947f11b492
Derek Prince <ace.prince08@gmail.com>
2016-06-17 16:00:42 -0600

    Gutted the old file. Pretty much just kept the lcd stuff and threw in the FreqMeasure lib stuff

--- src/LCD/LCD.ino
+++ src/LCD/LCD.ino
@@ -1,33 +1,38 @@
 // This is--presently--unfinished and untested.
 
 #include<LiquidCrystal.h> //Display lib
+#include<FreqMeasure.h>
 
-#define PIN_FREQ 2; //Using 2 because it can use ArrachInterrupt() and won't eat up a valuable PWM pin.
-
-uint32_t period = 0;  //I just want to make sure I know what these will be if I call them before r+w
-uint32_t freq =0;
+#define PIN_FREQ 8; //The lib actually defines this pin. Weird, but whatever. Non-PWM.
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //shifter to the latter pins to avoid pin 2, which is used for the read-in.
+uint64_t sum;
+uint8_t i;
 
 void setup() {
-  // initialization checks, probably. Check logic level of signal in, engine running or not, etc.
-  pinMode(pin, INPUT);  //reading period from pin(2) via PulseIn
-
   lcd.begin(16,2);  // I don't actually know the dimensions of my screen yet, but these are popular dimensions so why not.
                     // As for why on earth this library is column, row I will never know. Probably due to hardware-interface addressing but w/e.
+  Serial.begin(9600);   // for testing purposes. Should remember to remove this
+  FreqMeasure.begin();  // Hop-to!
 }
 
 void loop() {
-  //
-
-  //Might loop the loop and use a known number of pulseIn's to set the real-time duration of the loop.
-  period = pulseIn(PIN_FREQ, HIGH, 10000); // read period from high logic. Defaulf of 1 (s) is pretty terrible, I'll have to come up with something.
-                                      // 10000 = 10 ms sampling
-                                      // Also, I have to make sure that the wave I'm reading is actually at TTL levels. That's slightly important.
-  freq = 1/period;  //Might have to check against period == 0 or error handle the black hole.
-                    // This bit of code is also likely to disappear or be absorbed into the a functional implementation so I don't actually have to dedicate memory to it.
-                    //  ^ depends on how often I need to call freq for readout.
-                    // I could write a small algorithm to check the diff/period ratio and have it sample more frequently if the engine is accelerating.
-                    // This might be too expensive but it would make the write cycles like a billion times less intensive. Usually.
-
-  LiquidCrystal
+  // The old code that used to be here was garbage.
+  // Really I always knew it was temporary but like damn
+  if (FreqMeasure.available()) {
+    sum += FreqMeasure.read();
+    uint64_t currentTime = millis();
+    i++;
+    if ((currentTime - millis()) < 100) { //Just averages the period over a 100ms period. Helps with a dynamic range rather than a count measurement.
+    //if (i > 2) {  // testing code. 
+                    // Side note: letting it reach 4 would mean a double bit shift rather than the long division route.
+                    // I imagine the compiler would optimize this already, doubly so since it's float type but... Eh.
+      float frequency = FreqMeasure.countToFrequency(sum / i);  // float is great, I don't care about decimals anywho.
+      Serial.println(frequency);
+      test(leadLED, strip.Color(255,0,0));
+      
+      sum = 0;
+      i = 0;
+    }
+  }
+}
